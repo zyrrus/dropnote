12ef01730c9654e877dfa3dc6e3a75b2cc5df960
BlackDives <sirkamronwilson00@gmail.com>
2022-11-06 15:40:08 -0600

    Added search bar

--- lib/pages/discover_page.dart
+++ lib/pages/discover_page.dart
@@ -1,16 +1,26 @@
 import 'package:dropnote/theme.dart';
 import 'package:flutter/widgets.dart';
+import 'package:dropnote/widgets/search_bar.dart';
 
-class DiscoverPage extends StatelessWidget {
+class DiscoverPage extends StatefulWidget {
   const DiscoverPage({super.key});
 
   @override
+  State<DiscoverPage> createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
+  @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           "Discover",
           style: DropNote.textStyles.pageHeader(),
+        ),
+        SearchBar(
+          controller: TextEditingController(),
         )
       ],
     );
