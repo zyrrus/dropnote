bf9aef86ecdcad6aaebf60c4ba5b41f4e8c62e17
Zeke Abshire <zekeabshire@gmail.com>
2022-11-07 18:49:41 -0600

    cleaned test page

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,6 +1,4 @@
 import 'package:dropnote/pages/core_page.dart';
-import 'package:dropnote/widgets/file_card.dart';
-import 'package:dropnote/widgets/pdf_viewer.dart';
 import 'package:dropnote/theme.dart';
 import 'package:dropnote/widgets/top_bar.dart';
 import 'package:flutter/material.dart';
@@ -28,18 +26,6 @@ class _TestPageState extends State<TestPage> {
               color: DropNote.colors.foreground,
             ),
           ),
-          const FileCard(url: 'dropteam-product-strategy.pdf'),
-          // Expanded(
-          //   child: Container(
-          //     padding: const EdgeInsets.all(10.0),
-          //     decoration: BoxDecoration(
-          //       color: DropNote.colors.disabled,
-          //       borderRadius: BorderRadius.circular(5.0),
-          //     ),
-          //     child: const PDFViewer(url: "dropteam-product-strategy.pdf"),
-          //     // child: const PDFViewer(url: "dropnote.pdf"),
-          //   ),
-          // ),
         ],
       ),
     );
