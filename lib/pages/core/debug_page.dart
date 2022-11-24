b8be7f264803c10a8f54d15a9ac7a88431e63bfd
Zeke Abshire <zekeabshire@gmail.com>
2022-11-24 16:03:22 -0600

    Added view users + query users

--- lib/pages/core/debug_page.dart
+++ lib/pages/core/debug_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dropnote/theme.dart';
+import 'package:dropnote/widgets/avatar_list_item.dart';
 import 'package:dropnote/widgets/bar.dart';
 import 'package:dropnote/widgets/title_bar.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,8 @@ class DebugPage extends StatelessWidget {
             CreateNewUser(),
             Bar(),
             ViewAllUsers(),
+            Bar(),
+            ViewQueriedUsers(),
             Bar(),
             UploadFile(),
             Bar(),
@@ -46,6 +49,8 @@ class Collections {
   static String files = "files";
 }
 
+FirebaseFirestore db = FirebaseFirestore.instance;
+
 class User {
   // Make these private + remove named parameters
 
@@ -53,10 +58,15 @@ class User {
   final String email;
   final String school;
   final int? totalSaves;
-  final List<String>? uploadedFiles;
-  final List<String>? savedFiles;
+  final List<DocumentReference>? uploadedFiles;
+  final List<DocumentReference>? savedFiles;
 
-  User({
+  String get profilePicture {
+    String encodedName = Uri.encodeComponent(name.toLowerCase());
+    return "https://avatars.dicebear.com/api/bottts/$encodedName.svg";
+  }
+
+  const User({
     required this.name,
     required this.email,
     required this.school,
@@ -65,17 +75,30 @@ class User {
     this.savedFiles,
   });
 
-  String _getUrl() {
-    String toUrl(x) => "https://avatars.dicebear.com/api/bottts/$x.svg";
-    String encodedName = Uri.encodeComponent(name.toLowerCase());
-    return toUrl(encodedName);
+  factory User.fromJson(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return User(
+      name: data?['name'],
+      email: data?['email'],
+      school: data?['school'],
+      totalSaves: data?['totalSaves'],
+      uploadedFiles: data?['uploadedFiles'] is Iterable
+          ? List<DocumentReference>.from(data?['uploadedFiles'])
+          : null,
+      savedFiles: data?['savedFiles'] is Iterable
+          ? List<DocumentReference>.from(data?['savedFiles'])
+          : null,
+    );
   }
 
   Map<String, dynamic> toJson() => {
         "name": name,
         "email": email,
         "school": school,
-        "profilePicture": _getUrl(),
+        "profilePicture": profilePicture,
         "totalSaves": totalSaves ?? 0,
         "uploadedFiles": uploadedFiles ?? [],
         "savedFiles": savedFiles ?? [],
@@ -95,8 +118,6 @@ class _CreateNewUserState extends State<CreateNewUser> {
   final TextEditingController emailController = TextEditingController();
 
   void createUser() {
-    FirebaseFirestore db = FirebaseFirestore.instance;
-
     User user = User(
       name: nameController.text.trim(),
       school: schoolController.text.trim(),
@@ -138,14 +159,92 @@ class _CreateNewUserState extends State<CreateNewUser> {
 
 //
 
-class ViewAllUsers extends StatelessWidget {
+class ViewAllUsers extends StatefulWidget {
   const ViewAllUsers({super.key});
+
+  @override
+  State<ViewAllUsers> createState() => _ViewAllUsersState();
+}
+
+class _ViewAllUsersState extends State<ViewAllUsers> {
+  List<User> allUsers = [];
+  String? error;
+
+  void getAllUsers() {
+    db.collection(Collections.users).get().then(
+          (res) => setState(() {
+            allUsers = res.docs.map((e) => User.fromJson(e, null)).toList();
+          }),
+          onError: (e) => setState(() => error = "Error completing: $e"),
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         SubtitleBar(title: "ViewAllUsers"),
+        ...allUsers
+            .map((e) =>
+                AvatarListItem(imageURL: e.profilePicture, label: e.name))
+            .toList(),
+        ElevatedButton(onPressed: getAllUsers, child: Text("View All Users"))
+      ],
+    );
+  }
+}
+
+//
+
+class ViewQueriedUsers extends StatefulWidget {
+  const ViewQueriedUsers({super.key});
+
+  @override
+  State<ViewQueriedUsers> createState() => _ViewQueriedUsersState();
+}
+
+class _ViewQueriedUsersState extends State<ViewQueriedUsers> {
+  List<User> queriedUsers = [];
+  String? error;
+  final TextEditingController queryController = TextEditingController();
+
+  Future<List<User>> getAllUsers() async {
+    List<User> allUsers = [];
+    await db.collection(Collections.users).get().then(
+          (res) => allUsers.addAll(res.docs.map((e) => User.fromJson(e, null))),
+          onError: (e) => setState(() => error = "Error completing: $e"),
+        );
+
+    return allUsers;
+  }
+
+  Future<void> queryUsers() async {
+    var query = queryController.text.trim().toLowerCase();
+    var allUsers = await getAllUsers();
+    setState(() {
+      queriedUsers = allUsers
+          .where((user) =>
+              user.name.toLowerCase().contains(query) ||
+              user.school.toLowerCase().contains(query))
+          .toList();
+    });
+    queryController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SubtitleBar(title: "ViewQueriedUsers"),
+        ...queriedUsers
+            .map((e) =>
+                AvatarListItem(imageURL: e.profilePicture, label: e.name))
+            .toList(),
+        TextField(
+          decoration: InputDecoration(hintText: "Query", labelText: "Query"),
+          controller: queryController,
+        ),
+        ElevatedButton(onPressed: queryUsers, child: Text("Query Users"))
       ],
     );
   }
