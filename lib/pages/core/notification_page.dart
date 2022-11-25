573de8dcc51de50cf8b10c5a593ae3051bf5ee22
Zeke Abshire <zekeabshire@gmail.com>
2022-11-25 00:21:44 -0600

    Merged otis/branch and develop

--- lib/pages/core/notification_page.dart
+++ lib/pages/core/notification_page.dart
@@ -1,11 +1,8 @@
 import 'package:dropnote/theme.dart';
-import 'dart:math';
-import 'package:dropnote/widgets/avatar_list_item.dart';
-import 'package:dropnote/widgets/file_list_item.dart';
+import 'package:dropnote/widgets/bar.dart';
+import 'package:dropnote/widgets/notification_list_item.dart';
 import 'package:dropnote/widgets/title_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:dropnote/widgets/notification_list_item.dart/RequestItem.dart';
-import 'package:dropnote/widgets/notification_list_item.dart/SavedItem.dart';
 
 class NotificationPage extends StatelessWidget {
 // Author : Otis Jackson IV
@@ -16,76 +13,17 @@ class NotificationPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Text(
-            'Notifications',
-            style: DropNote.textStyles.main(
-              fontSize: 30.0,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
-        const Divider(),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20.0, 8.0, 0.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                child: Text(
-                  'Requests',
-                  style: DropNote.textStyles.main(
-                    fontSize: 23.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 11, 0, 0),
-                child: RequestItem(),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 11, 0, 0),
-                child: RequestItem(),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 11, 0, 0),
-                child: RequestItem(),
-              )
-            ],
-          ),
-        ),
-        const Divider(),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20.0, 8.0, 0.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                child: Text(
-                  'Saves',
-                  style: DropNote.textStyles.main(
-                    fontSize: 23.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 11, 0, 0),
-                child: SavedItem(),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 11, 0, 0),
-                child: SavedItem(),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 11, 0, 0),
-                child: SavedItem(),
-              )
-            ],
-          ),
-        ),
+        const TitleBar(title: "Notifications"),
+        const Bar(),
+        const SubtitleBar(title: "Requests"),
+        RequestListItem(who: "First Lastname", what: "Filename.txt"),
+        RequestListItem(who: "First Lastname", what: "Filename.txt"),
+        RequestListItem(who: "First Lastname", what: "Filename.txt"),
+        const Bar(),
+        const SubtitleBar(title: "Saves"),
+        NotificationListItem(text: "First Lastname saved Filename.txt"),
+        NotificationListItem(text: "First Lastname saved Filename.txt"),
+        NotificationListItem(text: "First Lastname saved Filename.txt"),
       ],
     );
   }
