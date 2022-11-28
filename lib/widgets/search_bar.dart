f0dae67ba0a8bf4a6169e5259c002e5c40897fa4
Zeke Abshire <zekeabshire@gmail.com>
2022-11-27 20:01:42 -0600

    Created text input field + updated search to use it

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:dropnote/widgets/text_input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:dropnote/theme.dart';
 
@@ -14,8 +15,6 @@ class SearchBar extends StatefulWidget {
 }
 
 class _SearchBarState extends State<SearchBar> {
-  Color focusedColor = DropNote.colors.lightGrey;
-  bool showClearButton = false;
   final List<String> animatedText = [
     "Search for documents",
     "Search for schools",
@@ -24,13 +23,6 @@ class _SearchBarState extends State<SearchBar> {
   ];
   int animationIndex = 0;
   Timer? timer;
-
-  void updateFocusedColor(bool hasFocus) {
-    setState(() {
-      focusedColor =
-          hasFocus ? DropNote.colors.foreground : DropNote.colors.lightGrey;
-    });
-  }
 
   void updateAnimationIndex() {
     setState(() {
@@ -57,56 +49,12 @@ class _SearchBarState extends State<SearchBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: DropNote.pagePadding),
-      child: SizedBox(
-        height: 40.0,
-        child: TextField(
-          textAlignVertical: TextAlignVertical.center,
-          controller: widget.controller,
-          style: DropNote.textStyles.main(
-            color: DropNote.colors.foreground,
-            fontWeight: FontWeight.w600,
-          ),
-          cursorColor: DropNote.colors.primary,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: DropNote.colors.lightGrey,
-            isCollapsed: true,
-            prefixIcon: Icon(
-              Icons.search,
-              color: DropNote.colors.foreground,
-            ),
-            alignLabelWithHint: true,
-            hintText: animatedText[animationIndex],
-            suffixIcon: IconButton(
-              onPressed: widget.controller.clear,
-              icon: Icon(
-                Icons.clear,
-                color: DropNote.colors.foreground,
-              ),
-            ),
-            hintStyle: DropNote.textStyles.main(
-              fontWeight: FontWeight.w600,
-              color: DropNote.colors.foreground,
-              fontSize: 18,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide(
-                color: DropNote.colors.lightGrey,
-                width: 1.0,
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide(
-                color: DropNote.colors.lightGrey,
-                width: 2.0,
-              ),
-            ),
-          ),
-        ),
+    return TextInputField(
+      controller: widget.controller,
+      label: animatedText[animationIndex],
+      prefixIcon: Icon(
+        Icons.search,
+        color: DropNote.colors.foreground,
       ),
     );
   }
