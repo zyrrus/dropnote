20fc67fdc062e6c33a26f52f457387f9303b4c2a
Zeke Abshire <zekeabshire@gmail.com>
2022-11-28 18:06:32 -0600

    created discover docs page

---
+++ lib/pages/discover_docs_page.dart
@@ -0,0 +1,77 @@
+import 'package:dropnote/api/users.dart';
+import 'package:dropnote/models/file.dart';
+import 'package:dropnote/models/user.dart';
+import 'package:dropnote/pages/core/core_page.dart';
+import 'package:dropnote/theme.dart';
+import 'package:dropnote/widgets/file_list_item.dart';
+import 'package:dropnote/widgets/title_bar.dart';
+import 'package:flutter/material.dart';
+
+class DiscoverDocsPage extends StatefulWidget {
+  final List<DNFile> docs;
+
+  const DiscoverDocsPage({Key? key, required this.docs}) : super(key: key);
+
+  @override
+  State<DiscoverDocsPage> createState() => _DiscoverDocsPageState();
+}
+
+class _DiscoverDocsPageState extends State<DiscoverDocsPage> {
+  Future<List<Widget>> getFiles() async {
+    var me = await UserAPI.getCurrent();
+
+    FileInfoStyle getStyle(String otherID) {
+      bool isMine = me.uploadedFiles?.contains(otherID) ?? false;
+      bool isSaved = me.savedFiles?.contains(otherID) ?? false;
+
+      if (isMine) {
+        return FileInfoStyle.uploadedDoc;
+      } else if (isSaved) {
+        return FileInfoStyle.deleteableDoc;
+      } else {
+        return FileInfoStyle.saveableDoc;
+      }
+    }
+
+    return widget.docs
+        .map((e) => Padding(
+              padding: EdgeInsets.only(bottom: DropNote.pagePadding),
+              child: FileListItem(
+                fileStyle: getStyle(e.fileID),
+                fileData: e,
+              ),
+            ))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CoreTemplate(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const TitleBar(title: "Documents", showBackButton: true),
+            FutureBuilder<List<Widget>>(
+              future: getFiles(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: DropNote.pagePadding),
+                    child: Column(children: snapshot.data!),
+                  );
+                }
+                return const Center(
+                  child: SizedBox.square(
+                    dimension: 100.0,
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
